3e8eafe6c9da953611939d614432f626fefc8854
Simon Maher <simonmaher8307@hotmail.com>
2019-11-26 10:31:03 +0200

    answered question 1 and 2

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1)  Use the var keyword to create a variable called myName.
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
+
+// ####################
+
+// 2 ) Assign the value 7 to variable a. Assign the contents of a to variable b.
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a; 
