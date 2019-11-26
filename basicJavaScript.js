38ea1fe75682c0a031dbb4998c30a1f058dd27a5
Simon Maher <simonmaher8307@hotmail.com>
2019-11-26 12:14:08 +0200

    answered question 19

--- basicJavaScript.js
+++ basicJavaScript.js
@@ -195,5 +195,18 @@ b *= 3;
 c *=10;
 
 // #################################
+
+// 19) Convert the assignments for a, b, and c to use the /= operator.
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Answer
+
+a /= 12;
+b /= 4;
+c /=11;
+
 // #################################
 
