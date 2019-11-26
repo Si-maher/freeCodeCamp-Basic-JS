ee525078adb1540752faaab62c8bf89e57e236a6
Simon Maher <simonmaher8307@hotmail.com>
2019-11-26 13:42:49 +0200

    answered question 21 and question 22

---
+++ basicJS-pt2.js
@@ -0,0 +1,32 @@
+// 21) Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see: 
+
+// Answer
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+console.log(myStr);
+
+
+// ################################
+
+// 22) Change the provided string to a string with single quotes at the beginning and end and no escape characters.
+
+// Answer
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr);
+
+// ################################
+
+
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
+// ################################
