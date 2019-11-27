0aabbeb8bbe0af59c590e20bdf11f89bbe67c01b
Simon Maher <simonmaher8307@hotmail.com>
2019-11-27 14:19:33 +0200

    answered question 55

--- basicJS-pt3.js
+++ basicJS-pt3.js
@@ -159,7 +159,15 @@ function nextInLine(arr, item) {
 
 // ###########################
 
+// 55) Modify the welcomeToBooleans function so that it returns true instead of false when the run button is clicked.
+
+function welcomeToBooleans() {
+
+    return true; 
+  }
+  
 // ###########################
+
 // ###########################
 // ###########################
 // ###########################
