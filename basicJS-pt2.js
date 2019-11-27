f5e31d9cb34f9afe1cee10fe5a522da48eceeb1d
Simon Maher <simonmaher8307@hotmail.com>
2019-11-27 10:21:13 +0200

    answered question 39

--- basicJS-pt2.js
+++ basicJS-pt2.js
@@ -167,5 +167,14 @@ var myArray = [18,64,99];
 myArray[0] = 45
 console.log(myArray);
 
+// ################################
+
+// 39) Using bracket notation select an element from myArray such that myData is equal to 8.
+
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Answer
+var myData = myArray[2][1];
+console.log(myData);
 
 // ################################
