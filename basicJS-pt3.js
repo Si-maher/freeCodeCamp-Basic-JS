b4b2d47ecd59dc3eaffcef93329eefcd06a1fbfd
Simon Maher <simonmaher8307@hotmail.com>
2019-11-27 11:19:42 +0200

    answered question 48

--- basicJS-pt3.js
+++ basicJS-pt3.js
@@ -61,6 +61,24 @@ function functionWithArgs(a, b) {
 
 // ###########################
 
+// 48) Using var, declare a global variable myGlobal outside of any function. Initialize it with a value of 10.Inside function fun1, assign 5 to oopsGlobal without using the var keyword.
+
+// Answer
+
+var myGlobal= 10;
+function fun1() {
+oopsGlobal = 5;
+}
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
 
 // ###########################
 // ###########################
