baa477020fc8fdd52b3fccd4c0bf8424b98b0ebf
Simon Maher <simonmaher8307@hotmail.com>
2019-12-09 15:52:06 +0200

    answered question 72

--- basicJS-pt4.js
+++ basicJS-pt4.js
@@ -175,8 +175,39 @@ function orderMyLogic(val) {
     }
   }
   orderMyLogic(6);
+
 // #################################
+
+// 71) Write chained if/else if statements to fulfill the following conditions:
+
+// num < 5 - return "Tiny"
+// num < 10 - return "Small"
+// num < 15 - return "Medium"
+// num < 20 - return "Large"
+// // num >= 20 - return "Huge"
+
+// Answer
+
+function testSize(num) {
+  if (num < 5){
+    return "Tiny"
+  }else if(num < 10) {
+    return "Small"
+  }else if(num < 15) {
+    return "Medium"
+  }else if( num < 20) {
+    return "Large"
+  }else if(num >= 20) {
+    return "Huge"
+  }
+  
+    return "Change Me";
+  }
+  testSize(15);
+
 // #################################
+
+72) 
 // #################################
 // #################################
 // #################################
