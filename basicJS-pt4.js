0df4469863dce90a8e8718f23dfdae82c7967fc6
Simon Maher <simonmaher8307@hotmail.com>
2019-12-09 14:30:15 +0200

    answered question 62

--- basicJS-pt4.js
+++ basicJS-pt4.js
@@ -26,7 +26,21 @@ function testStrictNotEqual(val) {
 
 // #################################
 
+// 62) Add the greater than operator to the indicated lines so that the return statements make sense.
 
+// Answer
+
+function testGreaterThan(val) {
+    if (val > 100) {  
+      return "Over 100";
+    }
+    if (val > 10) { 
+      return "Over 10";
+    }
+  return "10 or Under";
+  }
+  testGreaterThan(11);
+  
 // #################################
 // #################################
 // #################################
