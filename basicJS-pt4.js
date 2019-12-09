1d641916ccb673d9e7cf69bc999b0dc1f3a2716f
Simon Maher <simonmaher8307@hotmail.com>
2019-12-09 14:34:54 +0200

    answered question 64

--- basicJS-pt4.js
+++ basicJS-pt4.js
@@ -60,7 +60,20 @@ function testGreaterOrEqual(val) {
 
 // #################################
 
-64)
+// 64) Add the less than operator to the indicated lines so that the return statements make sense.
+
+// Answer
+
+function testLessThan(val) {
+    if (val < 25) { 
+      return "Under 25";
+    }
+   if (val < 55) {
+      return "Under 55";
+    }
+ return "55 or Over";
+  }
+  testLessThan(11);
 // #################################
 // #################################
 // #################################
