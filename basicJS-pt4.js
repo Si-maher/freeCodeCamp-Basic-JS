b5d1773e46d3b190e78d69137f40c2759b2c376a
Simon Maher <simonmaher8307@hotmail.com>
2019-12-09 15:46:35 +0200

    answered question 70

--- basicJS-pt4.js
+++ basicJS-pt4.js
@@ -161,7 +161,20 @@ function testElseIf(val) {
 
 // #################################
 
-70)
+// 70) Change the order of logic in the function so that it will return the correct statements in all cases.
+
+// Answer
+
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  orderMyLogic(6);
 // #################################
 // #################################
 // #################################
