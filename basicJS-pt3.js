c2ec64a714bc388e1a88009a6047eb22fb63dccb
Simon Maher <simonmaher8307@hotmail.com>
2019-12-09 14:03:17 +0200

    answered question 59

--- basicJS-pt3.js
+++ basicJS-pt3.js
@@ -211,6 +211,18 @@ function testStrict(val) {
 testStrict(7);
 
 // ###########################
+
+// 59) The compareEquality function in the editor compares two values using the equality operator. Modify the function so that it returns "Equal" only when the values are strictly equal.
+
+// Answer
+
+function compareEquality(a, b) {
+  if (a === b) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+compareEquality(10, 10);
 // ###########################
 // ###########################
 // ###########################
