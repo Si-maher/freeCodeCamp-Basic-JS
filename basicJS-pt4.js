14cac9077abab022794b935f1bdddb17a4f1be09
Simon Maher <simonmaher8307@hotmail.com>
2019-12-09 18:26:29 +0200

    answered question 75

--- basicJS-pt4.js
+++ basicJS-pt4.js
@@ -295,8 +295,39 @@ function switchOfStuff(val) {
 
 // #################################
 
-75) 
+// 75) Write a switch statement to set answer for the following ranges:
+// 1-3 - "Low"
+// 4-6 - "Mid"
+// 7-9 - "High"
+
+// Answer 
+
+function sequentialSizes(val) {
+    var answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+    answer = "Low";
+    break;
+    case 4:
+    case 5:
+    case 6:
+    answer = "Mid";
+    break;
+    case 7:
+    case 8:
+    case 9:
+    answer = "High";
+    break;
+  }
+    return answer;
+  }
+  console.log(sequentialSizes(1));
+
 // #################################
+
+
 // #################################
 // #################################
 // #################################
