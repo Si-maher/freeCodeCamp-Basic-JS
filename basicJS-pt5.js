68ac47219d4c277969059eac2e8a4625151ff043
Simon Maher <simonmaher8307@hotmail.com>
2019-12-10 11:39:52 +0200

    answered question 87

--- basicJS-pt5.js
+++ basicJS-pt5.js
@@ -100,7 +100,27 @@ var myDog = {
 
 // ##################################
 
+// 87) Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
+
+// Answer 
+
+function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+       "alpha":"Adams",
+      "bravo":"Boston",
+      "charlie":"Chicago",
+      "delta":"Denver",
+      "echo":"Easy",
+       "foxtrot":"Frank",
+    }
+         result = lookup[val]
+    return result;
+  }
+  console.log(phoneticLookup("Easy"));
+
 // ##################################
+
 // ##################################
 // ##################################
 // ##################################
