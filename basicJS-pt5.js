9ebf0c56cca776933b91c1a7b7d43045c72b2b50
Simon Maher <simonmaher8307@hotmail.com>
2019-12-10 10:48:14 +0200

    answered question 81

--- basicJS-pt5.js
+++ basicJS-pt5.js
@@ -12,7 +12,20 @@ var myDog = {
 
 // ##################################
 
+// 81) Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object's property hat and set the variable shirtValue equal to the object's property shirt.
 
+// Answer 
+
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+  };
+  var hatValue = testObj.hat;
+  var shirtValue = testObj.shirt; 
+  
+  console.log(hatValue)
+  
 // ##################################
 // ##################################
 // ##################################
