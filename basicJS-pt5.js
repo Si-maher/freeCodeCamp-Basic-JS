daddb1a691f33ab1bc3ab7cb332235e79616b4ca
Simon Maher <simonmaher8307@hotmail.com>
2019-12-10 16:58:19 +0200

    answered question 89

--- basicJS-pt5.js
+++ basicJS-pt5.js
@@ -138,9 +138,44 @@ var myObj = {
     return "Not Found";
   }
     }
-  checkObj("gift");
+  console.log(checkObj("gift"));
+
 // ##################################
+
+// 89) Add a new album to the myMusic array. Add artist and title strings, release_year number, and a formats array of strings.
+
+// Answer 
+
+var myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "AC DC",
+      "title": "ThunderStruck",
+      "release_year": 1985,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+
+  console.log(myMusic)
+
 // ##################################
+
+
 // ##################################
 // ##################################
 // ##################################
