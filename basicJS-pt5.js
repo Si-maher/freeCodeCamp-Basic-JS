fe2cf6781ebc108657e1915ee3873767ee9754c6
Simon Maher <simonmaher8307@hotmail.com>
2019-12-10 11:53:07 +0200

    answered question 88

--- basicJS-pt5.js
+++ basicJS-pt5.js
@@ -121,6 +121,24 @@ function phoneticLookup(val) {
 
 // ##################################
 
+// 88) Modify the function checkObj to test myObj for checkProp. If the property is found, return that property's value. If not, return "Not Found".
+
+// Answer 
+
+var myObj = {
+    gift: "pony",
+    pet: "kitten",
+    bed: "sleigh"
+  };
+  
+  function checkObj(checkProp) {
+  if(myObj.hasOwnProperty(checkProp) == true){
+    return myObj[checkProp];
+  }else {
+    return "Not Found";
+  }
+    }
+  checkObj("gift");
 // ##################################
 // ##################################
 // ##################################
