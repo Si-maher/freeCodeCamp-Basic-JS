d1fccd943baf090abb76c8c9f2cf0d0434a6f543
Simon Maher <simonmaher8307@hotmail.com>
2019-12-10 11:16:03 +0200

    answered question 84

--- basicJS-pt5.js
+++ basicJS-pt5.js
@@ -57,7 +57,19 @@ var testObj = {
   
 // ##################################
 
+// 84) Update the myDog object's name property. Let's change her name from "Coder" to "Happy Coder". You can use either dot or bracket notation.
 
+// Answer 
+
+var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.name = "Happy Coder"
+
+// 
 // ##################################
 // ##################################
 // ##################################
