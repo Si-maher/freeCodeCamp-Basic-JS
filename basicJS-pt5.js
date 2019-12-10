126c6c0e4cd5ec9073f19e9940dfc9d16537285b
Simon Maher <simonmaher8307@hotmail.com>
2019-12-10 10:57:15 +0200

    answered question 82

--- basicJS-pt5.js
+++ basicJS-pt5.js
@@ -25,9 +25,24 @@ var testObj = {
   var shirtValue = testObj.shirt; 
   
   console.log(hatValue)
-  
+
 // ##################################
+
+// 82) Read the values of the properties "an entree" and "the drink" of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+
+// Answer
+
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+  };
+  var entreeValue = testObj["an entree"];   
+  var drinkValue = testObj["the drink"]; 
+
 // ##################################
+
+
 // ##################################
 // ##################################
 // ##################################
