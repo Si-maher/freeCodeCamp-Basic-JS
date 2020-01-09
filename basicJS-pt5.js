7303e9a7c784a46a15f715c206611c183827fda8
Simon Maher <simonmaher8307@hotmail.com>
2020-01-09 18:39:37 +0200

    answered question 99

--- basicJS-pt5.js
+++ basicJS-pt5.js
@@ -372,3 +372,18 @@ for (var i = 0; i < arr.length; i++){
 multiplyAll([[1],[2],[3]]);
 
 // ##################################
+
+// 99) Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+
+// Answer 
+
+var myArray = [];
+var i = 10;
+do {
+  myArray.push(i)
+  i++
+}while(i < 10)
+
+console.log(myArray);
+
+// ##################################
